cc07ea983f2b8b4a9d773cec15c94470ba0c4cae
Wyvernslay3r <ethanboatright@gmail.com>
2022-04-11 20:24:47 -0400

    Added functionality to sniff on port 80. Added funct to pull images out and write them to disk. Future funct to include:     -Arbitraty ports     -Proxy integration for HTTPS modified:   chapter4/recapper.py

--- chapter4/recapper.py
+++ chapter4/recapper.py
@@ -1,3 +1,4 @@
+from requests import Response
 from scapy.all import TCP, rdpcap
 import collections
 import os
@@ -18,9 +19,60 @@ def get_header(payload):
         sys.stdout.flush()
         return None
 
+    #Use Regex to split headers 
     header = dict(re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', raw_header.decode()))
 
+    #Look for key Content Type in header=
     if 'Content-Type' not in header:
         return None
     
     return header
+
+
+def extract_content(Response: response, content_name='image'):
+    content, content_type = None, None
+    
+    if content_name in Response.header['Content-Type']:
+        content_type = Response.header['Content-Type'].split('/')[1]
+        content = Response.payload[Response.payload.index(b'\r\n\r\n')+4:]
+
+        if 'Content-Encoding' in Response.header:
+            if Response.header['Content-Encoding'] == "gzip":
+                content = zlib.decompress(Response.payload, zlib.MAX_WBITS |32 )
+            elif Response.header['Content-Encoding'] == "deflate":
+                content = zlib.decompress(Response.payload)
+    
+    return content, content_type
+
+class Recapper:
+    def __init__(self, fname):
+        pcap = rdpcap(fname)
+        self.sessions = pcap.sessions()
+        self.responses = list()
+
+    def get_responses(self):
+        for session in self.sessions:
+            payload = b''
+            for packet in self.sessions[session]:
+                try:
+                    if packet[TCP].dport == 80 or packet[TCP].sport == 80:
+                        payload += bytes (packet[TCP].payload)
+                except IndexError:
+                    sys.stdout.write('x')
+                    sys.stdout.flush()
+
+            if payload:
+                header = get_header(payload)
+                if header is None:
+                    continue
+                self.responses.append(Response(header=header, payload=payload))
+
+    def write(self, content_name):
+        for i, response in enumerate(self.responses):
+            content, content_type = extract_content(response, content_name)
+            if content and content_type:
+                fname = os.path.join(OUTDIR, f'ex_{i}.{content_type}')
+                print(f'Writing {fname}')
+
+                with open(fname, 'wb') as f:
+                    f.write(content)
